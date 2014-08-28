87e8c2941e34d3638dc566421b85f37a7cb507d5
DerRomtester <dieb.stefan.96@gmail.com>
2014-08-28 11:06:40 +0200

    build script

---
+++ make.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ "$CPU_JOB_NUM" = "" ] ; then
+CPU_JOB_NUM=`grep -c processor /proc/cpuinfo`
+fi
+echo "Making oneplus one kernel"
+DATE_START=$(date +"%s")
+
+export ARCH=arm
+export SUBARCH=arm
+make franco_defconfig
+make -j$CPU_JOB_NUM
+echo "End of compiling kernel!"
+
+DATE_END=$(date +"%s")
+echo
+DIFF=$(($DATE_END - $DATE_START))
+echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
