6a42584015b5b976cbda952d06429f19c36f2750
DerRomtester <dieb.stefan.96@gmail.com>
2014-11-16 21:47:26 +0100

    lmk: use swapfree for bettery memory reclaim idea from faux123

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -377,7 +377,8 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	if (global_page_state(NR_SHMEM) + total_swapcache_pages <
 		global_page_state(NR_FILE_PAGES))
 		other_file = global_page_state(NR_FILE_PAGES) -
-						global_page_state(NR_SHMEM) -
+						global_page_state(NR_SHMEM) 
+						(si.freeswap >> 1) -
 						total_swapcache_pages;
 	else
 		other_file = 0;
