8472830b70537ab1b47ef9725e525cea48db2e4c
DerRomtester <dieb.stefan.96@gmail.com>
2014-11-18 21:57:11 +0100

    cpufreq: Introduce new relation for freq selection Introduce CPUFREQ_RELATION_C for frequency selection. It selects the frequency with the minimum euclidean distance to target. In case of equal distance between 2 frequencies, it will select the greater frequency. Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr> Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/freq_table.c
+++ drivers/cpufreq/freq_table.c
@@ -101,6 +101,10 @@ int cpufreq_frequency_table_target(struct cpufreq_policy *policy,
 	struct cpufreq_frequency_table suboptimal = {
 		.index = ~0,
 		.frequency = 0,
+
+	struct cpufreq_frequency_table *pos;
+		unsigned int freq, i = 0;
+		unsigned int freq, diff, i = 0;
 	};
 	unsigned int i;
 
@@ -112,6 +116,7 @@ int cpufreq_frequency_table_target(struct cpufreq_policy *policy,
 		suboptimal.frequency = ~0;
 		break;
 	case CPUFREQ_RELATION_L:
+	case CPUFREQ_RELATION_C:
 		optimal.frequency = ~0;
 		break;
 	}
@@ -149,6 +154,15 @@ int cpufreq_frequency_table_target(struct cpufreq_policy *policy,
 				}
 			}
 			break;
+		case CPUFREQ_RELATION_C:
+			diff = abs(freq - target_freq);
+			if (diff < optimal.frequency ||
+				(diff == optimal.frequency &&
+				freq > table[optimal.driver_data].frequency)) {
+					optimal.frequency = diff;
+					optimal.driver_data = i;
+			}
+break;
 		}
 	}
 	if (optimal.index > i) {
