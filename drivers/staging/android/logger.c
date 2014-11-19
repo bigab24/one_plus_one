4efbf6900a514729a09cdf34bbf1a2478aff6461
DerRomtester <dieb.stefan.96@gmail.com>
2014-11-19 17:25:44 +0100

    Revert "staging: android: logger: enhanced logcat logging v3"
    This reverts commit 6e136e73505f94facb692f962ee4d02504942f2e.

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -29,29 +29,23 @@
 #include <linux/time.h>
 #include <linux/vmalloc.h>
 #include "logger.h"
-#ifdef CONFIG_LCD_NOTIFY
-#include <linux/lcd_notify.h>
-#elif defined(CONFIG_POWERSUSPEND)
 #include <linux/powersuspend.h>
-#endif
+
 #include <asm/ioctls.h>
 
 #ifndef CONFIG_LOGCAT_SIZE
-#define CONFIG_LOGCAT_SIZE 256
+#define CONFIG_LOGCAT_SIZE 128
 #endif
 
-#if defined(CONFIG_LCD_NOTIFY) || defined(CONFIG_POWERSUSPEND)
 /*
- * 0 - Enabled, 1 - Auto Suspend, 2 - Disabled
+ * 0 - Enabled
+ * 1 - Auto Suspend
+ * 2 - Disabled
  */
-static unsigned int log_mode = 2; // Disabled by default
+static unsigned int log_mode = 2;
 static unsigned int log_enabled = 1; // Do not change this value
-module_param(log_mode, uint, S_IWUSR | S_IRUGO);
 
-#ifdef CONFIG_LCD_NOTIFY
-static struct notifier_block notif;
-#endif
-#endif
+module_param(log_mode, uint, S_IWUSR | S_IRUGO);
 
 /**
  * struct logger_log - represents a specific log, such as 'main' or 'radio'
@@ -128,10 +122,9 @@ static inline struct logger_log *file_get_log(struct file *file)
 {
 	if (file->f_mode & FMODE_READ) {
 		struct logger_reader *reader = file->private_data;
-
 		return reader->log;
-	}
-	return file->private_data;
+	} else
+		return file->private_data;
 }
 
 /*
@@ -178,7 +171,8 @@ static size_t get_user_hdr_len(int ver)
 {
 	if (ver < 2)
 		return sizeof(struct user_logger_entry_compat);
-	return sizeof(struct logger_entry);
+	else
+		return sizeof(struct logger_entry);
 }
 
 static ssize_t copy_header_to_user(int ver, struct logger_entry *entry,
@@ -480,50 +474,21 @@ static ssize_t do_write_log_from_user(struct logger_log *log,
 	return count;
 }
 
-#if defined(CONFIG_LCD_NOTIFY) || defined(CONFIG_POWERSUSPEND)
-#ifdef CONFIG_LCD_NOTIFY
-static void log_suspend(void)
-#elif defined(CONFIG_POWERSUSPEND)
-static void log_suspend(struct power_suspend *handler)
-#endif
+static void log_early_suspend(struct power_suspend *handler)
 {
 	if (log_mode == 1)
 		log_enabled = 0;
 }
 
-#ifdef CONFIG_LCD_NOTIFY
-static void log_resume(void)
-#elif defined(CONFIG_POWERSUSPEND)
-static void log_resume(struct power_suspend *handler)
-#endif
+static void log_late_resume(struct power_suspend *handler)
 {
 	log_enabled = 1;
 }
 
-#ifdef CONFIG_LCD_NOTIFY
-static int lcd_notifier_callback(struct notifier_block *this,
-				unsigned long event, void *data)
-{
-	switch (event) {
-	case LCD_EVENT_ON_END:
-		log_resume();
-		break;
-	case LCD_EVENT_OFF_END:
-		log_suspend();
-		break;
-	default:
-		break;
-	}
-
-	return NOTIFY_OK;
-}
-#elif defined(CONFIG_POWERSUSPEND)
-static struct power_suspend log_power_suspend = {
-	.suspend = log_suspend,
-	.resume = log_resume,
+static struct power_suspend log_suspend = {
+	.suspend = log_early_suspend,
+	.resume = log_late_resume,
 };
-#endif
-#endif
 
 /*
  * logger_aio_write - our write method, implementing support for write(),
@@ -538,10 +503,8 @@ static ssize_t logger_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	struct logger_entry header;
 	struct timespec now;
 
-#if defined(CONFIG_LCD_NOTIFY) || defined(CONFIG_POWERSUSPEND)
 	if (!log_enabled || log_mode == 2)
 		return 0;
-#endif
 
 	log = file_get_log(iocb->ki_filp);
 	now = current_kernel_time();
@@ -858,16 +821,13 @@ static int __init create_log(char *log_name, int size)
 	if (unlikely(ret)) {
 		pr_err("failed to register misc device for log '%s'!\n",
 				log->misc.name);
-		goto out_free_misc_name;
+		goto out_free_log;
 	}
 
 	pr_info("created %luK log '%s'\n",
 		(unsigned long) log->size >> 10, log->misc.name);
 
 	return 0;
-
-out_free_misc_name:
-	kfree(log->misc.name);
 
 out_free_log:
 	kfree(log);
@@ -881,16 +841,7 @@ static int __init logger_init(void)
 {
 	int ret;
 
-#ifdef CONFIG_LCD_NOTIFY
-	notif.notifier_call = lcd_notifier_callback;
-	ret = lcd_register_client(&notif);
-        if (ret != 0) {
-                pr_err("logger: Failed to register LCD notifier callback\n");
-		goto out;
-	}
-#elif defined(CONFIG_POWERSUSPEND)
-	register_power_suspend(&log_power_suspend);
-#endif
+	register_power_suspend(&log_suspend);
 
 	ret = create_log(LOGGER_LOG_MAIN, CONFIG_LOGCAT_SIZE*1024);
 	if (unlikely(ret))
@@ -924,12 +875,6 @@ static void __exit logger_exit(void)
 		list_del(&current_log->logs);
 		kfree(current_log);
 	}
-#ifdef CONFIG_LCD_NOTIFY
-	lcd_unregister_client(&notif);
-	notif.notifier_call = NULL;
-#elif defined(CONFIG_POWERSUSPEND)
-	unregister_power_suspend(&log_power_suspend);
-#endif
 }
 
 
