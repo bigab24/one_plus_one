ffb0e01425d144aed91e396f8f1567cf8569d996
Fürst <dieb.stefan.96@gmail.com>
2014-12-13 16:48:06 +0100

    cpufreq: remove zzmove it never worked for me
    Conflicts:
    	arch/arm/configs/Tyr_defconfig
    	drivers/cpufreq/cpufreq_zzmoove.c
    	drivers/cpufreq/cpufreq_zzmoove_profiles.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -411,9 +411,6 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SLIM)
 extern struct cpufreq_governor cpufreq_gov_slim;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_slim)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
-extern struct cpufreq_governor cpufreq_gov_zzmoove;
-#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_zzmoove)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
 extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_ondemandplus)
