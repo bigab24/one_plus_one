NamelessRom/android_kernel_oneplus_msm8974

mako_hotplug: toggle able screen off maximum frequency
Change-Id: I804b1e357417575c3ae32dc50f9009a71197046e

arch/arm/kernel/mako_hotplug.c
@@ -106,6 +106,11 @@ struct hotplug_tunables {
 	 * per second it runs
 	 */
 	unsigned int timer;
+
+	/**
+	 * the maximum frequency when screen is turned off.
+	 */
+	unsigned int screen_off_max;
 } tunables;
 
 static struct workqueue_struct *wq;
@@ -280,7 +285,9 @@ static struct notifier_block cpufreq_notifier = {
 
 static void screen_off_max_freq(int cpu, bool lower_max_freq)
 {
-	stats.freq = lower_max_freq ? MAX_FREQ_CAP : stats.saved_freq;
+	struct hotplug_tunables *t = &tunables;
+
+	stats.freq = lower_max_freq ? t->screen_off_max : stats.saved_freq;
 
 	/*
 	 * This can be 0 on bootup if policy->max is not yet set
@@ -295,7 +302,7 @@ static void screen_off_max_freq(int cpu, bool lower_max_freq)
 	 * If you can reproduce it contact me (/proc/kmsg shows the log for that)
 	 */
 	if (!lower_max_freq) {
-		if (stats.freq <= MAX_FREQ_CAP)
+		if (stats.freq <= t->screen_off_max)
 			stats.freq = LONG_MAX;
 	}
 
@@ -558,6 +565,30 @@ static ssize_t timer_store(struct device *dev, struct device_attribute *attr,
 	return size;
 }
 
+static ssize_t screen_off_max_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct hotplug_tunables *t = &tunables;
+
+	return snprintf(buf, PAGE_SIZE, "%u\n", t->screen_off_max);
+}
+
+static ssize_t screen_off_max_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+	struct hotplug_tunables *t = &tunables;
+	int ret;
+	unsigned long new_val;
+
+	ret = kstrtoul(buf, 0, &new_val);
+	if (ret < 0)
+		return ret;
+
+	t->screen_off_max = new_val > ULONG_MAX ? ULONG_MAX : new_val;
+
+	return size;
+}
+
 static DEVICE_ATTR(enabled, 0664, make_hotplug_enabled_show,
 		make_hotplug_enabled_store);
 static DEVICE_ATTR(load_threshold, 0664, load_threshold_show,
@@ -571,6 +602,8 @@ static DEVICE_ATTR(cpufreq_unplug_limit, 0664, cpufreq_unplug_limit_show,
 static DEVICE_ATTR(min_time_cpu_online, 0664, min_time_cpu_online_show,
 		min_time_cpu_online_store);
 static DEVICE_ATTR(timer, 0664, timer_show, timer_store);
+static DEVICE_ATTR(screen_off_max, 0664, screen_off_max_show,
+		screen_off_max_store);
 
 static struct attribute *mako_hotplug_control_attributes[] = {
 	&dev_attr_enabled.attr,
@@ -580,6 +613,7 @@ static struct attribute *mako_hotplug_control_attributes[] = {
 	&dev_attr_cpufreq_unplug_limit.attr,
 	&dev_attr_min_time_cpu_online.attr,
 	&dev_attr_timer.attr,
+	&dev_attr_screen_off_max.attr,
 	NULL
 };
 
@@ -615,6 +649,7 @@ static int __devinit mako_hotplug_probe(struct platform_device *pdev)
 	t->cpufreq_unplug_limit = DEFAULT_CPUFREQ_UNPLUG_LIMIT;
 	t->min_time_cpu_online = DEFAULT_MIN_TIME_CPU_ONLINE;
 	t->timer = DEFAULT_TIMER;
+	t->screen_off_max = MAX_FREQ_CAP;
 
 	stats.notif.notifier_call = lcd_notifier_callback;
 

bigab24/one_plus_one

arch/arm/kernel/mako_hotplug.c
@@ -38,11 +38,11 @@
 #define DEFAULT_CPUFREQ_UNPLUG_LIMIT 1497600
 #define DEFAULT_MIN_TIME_CPU_ONLINE 1
 #define DEFAULT_TIMER 1
+#define DEFAULT_MAX_FREQ_CAP 1036800
 
 #define MIN_CPU_UP_US (1000 * USEC_PER_MSEC)
 #define NUM_POSSIBLE_CPUS num_possible_cpus()
 #define HIGH_LOAD (90 << 1)
-#define MAX_FREQ_CAP 1036800
 
 struct cpu_stats {
 	unsigned int counter;
@@ -106,6 +106,11 @@ struct hotplug_tunables {
 	 * per second it runs
 	 */
 	unsigned int timer;
+
+	/**
+	 * the maximum frequency when screen is turned off.
+	 */
+	unsigned int screen_off_max;
 } tunables;
 
 static struct workqueue_struct *wq;
@@ -280,7 +285,9 @@ static struct notifier_block cpufreq_notifier = {
 
 static void screen_off_max_freq(int cpu, bool lower_max_freq)
 {
-	stats.freq = lower_max_freq ? MAX_FREQ_CAP : stats.saved_freq;
+	struct hotplug_tunables *t = &tunables;
+
+	stats.freq = lower_max_freq ? t->screen_off_max : stats.saved_freq;
 
 	/*
 	 * This can be 0 on bootup if policy->max is not yet set
@@ -295,7 +302,7 @@ static void screen_off_max_freq(int cpu, bool lower_max_freq)
 	 * If you can reproduce it contact me (/proc/kmsg shows the log for that)
 	 */
 	if (!lower_max_freq) {
-		if (stats.freq <= MAX_FREQ_CAP)
+		if (stats.freq <= t->screen_off_max)
 			stats.freq = LONG_MAX;
 	}
 
@@ -558,6 +565,30 @@ static ssize_t timer_store(struct device *dev, struct device_attribute *attr,
 	return size;
 }
 
+static ssize_t screen_off_max_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct hotplug_tunables *t = &tunables;
+
+	return snprintf(buf, PAGE_SIZE, "%u\n", t->screen_off_max);
+}
+
+static ssize_t screen_off_max_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+	struct hotplug_tunables *t = &tunables;
+	int ret;
+	unsigned long new_val;
+
+	ret = kstrtoul(buf, 0, &new_val);
+	if (ret < 0)
+		return ret;
+
+	t->screen_off_max = new_val > ULONG_MAX ? ULONG_MAX : new_val;
+
+	return size;
+}
+
 static DEVICE_ATTR(enabled, 0664, make_hotplug_enabled_show,
 		make_hotplug_enabled_store);
 static DEVICE_ATTR(load_threshold, 0664, load_threshold_show,
@@ -571,6 +602,8 @@ static DEVICE_ATTR(cpufreq_unplug_limit, 0664, cpufreq_unplug_limit_show,
 static DEVICE_ATTR(min_time_cpu_online, 0664, min_time_cpu_online_show,
 		min_time_cpu_online_store);
 static DEVICE_ATTR(timer, 0664, timer_show, timer_store);
+static DEVICE_ATTR(screen_off_max, 0664, screen_off_max_show,
+		screen_off_max_store);
 
 static struct attribute *mako_hotplug_control_attributes[] = {
 	&dev_attr_enabled.attr,
@@ -580,6 +613,7 @@ static struct attribute *mako_hotplug_control_attributes[] = {
 	&dev_attr_cpufreq_unplug_limit.attr,
 	&dev_attr_min_time_cpu_online.attr,
 	&dev_attr_timer.attr,
+	&dev_attr_screen_off_max.attr,
 	NULL
 };
 
@@ -615,6 +649,7 @@ static int __devinit mako_hotplug_probe(struct platform_device *pdev)
 	t->cpufreq_unplug_limit = DEFAULT_CPUFREQ_UNPLUG_LIMIT;
 	t->min_time_cpu_online = DEFAULT_MIN_TIME_CPU_ONLINE;
 	t->timer = DEFAULT_TIMER;
+	t->screen_off_max = DEFAULT_MAX_FREQ_CAP;
 
 	stats.notif.notifier_call = lcd_notifier_callback;
 